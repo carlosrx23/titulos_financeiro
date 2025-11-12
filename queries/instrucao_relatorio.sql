3d951f027f2dd34c0be2f94ca51c1f1eb3a03074
Carlos Ribeiro <143560855+carlosrx23@users.noreply.github.com>
2025-11-12 09:44:00 -0300

    Update instrucao_relatorio.sql
    Remoção de algumas informações sensíveis

--- queries/instrucao_relatorio.sql
+++ queries/instrucao_relatorio.sql
@@ -1,60 +1,48 @@
 /*
-  Query: Instrução de relatório financeiro
+  Query: Relatório de Títulos em Aberto (exemplo técnico)
   Autor: Carlos Ribeiro
   Descrição:
-    Essa instrução é a query executada pelo gerador de relatórios da aplicação.
-    Ela consome a view `PASV_TITULOS_EM_ABERTO_COMPRADOR`, aplicando parâmetros
-    dinâmicos (intervalo de datas, empresa, situação e comprador) e retornando
-    os dados formatados para exibição.
+    Exemplo de instrução SQL utilizada em relatórios financeiros.
+    O script aplica parâmetros dinâmicos (intervalo de datas, empresa,
+    situação e comprador) e retorna dados formatados para exibição.
 */
-
 
 SELECT 
     T0.*, 
-    TO_CHAR(:DT1, 'DD/MM/YYYY') AS INICIO,
-    TO_CHAR(:DT2, 'DD/MM/YYYY') AS FIM,    
+    TO_CHAR(:DT_INICIO, 'DD/MM/YYYY') AS DATA_INICIAL,
+    TO_CHAR(:DT_FIM, 'DD/MM/YYYY') AS DATA_FINAL,
     CASE 
-        WHEN :LS3 = 'A' THEN 'ABERTO'
-        WHEN :LS3 = 'Q' THEN 'QUITADO'
+        WHEN :SITUACAO = 'A' THEN 'ABERTO'
+        WHEN :SITUACAO = 'Q' THEN 'QUITADO'
         ELSE 'ABERTO E QUITADO'
-    END AS SITUACAO_ESCOLHIDA, 
-    :LS1 AS ESCOLHA_EMPRESA,
+    END AS STATUS_FILTRADO, 
+    :EMPRESA AS EMPRESA_FILTRADA,
     CASE
-        WHEN :LS4 = 'TODOS' THEN 'TODOS'
-        ELSE UPPER(:LS4)
-    END AS ESCOLHA_COMPRADOR,
-    TO_CHAR(SYSDATE, 'DD/MM/YYYY HH24:MI:SS') AS DATA_HORA_IMPRESSAO,  
-        
-    (SELECT 
-         SUM(T1.VLRORIGINAL) 
-     FROM PASV_TITULOS_EM_ABERTO_COMPRADOR T1
-     WHERE T1.CONTA_CORRENTE = T0.CONTA_CORRENTE
-       AND T1.DTAPROGRAMADA BETWEEN :DT1 AND :DT2
-    ) AS TOTAL_VALOR_ORIGINAL,
-    :NR2 as SUPRIMIR
+        WHEN :COMPRADOR = 'TODOS' THEN 'TODOS'
+        ELSE UPPER(:COMPRADOR)
+    END AS COMPRADOR_FILTRADO,
+    TO_CHAR(SYSDATE, 'DD/MM/YYYY HH24:MI:SS') AS DATA_HORA_GERACAO,
 
-FROM PASV_TITULOS_EM_ABERTO_COMPRADOR T0
+    (SELECT SUM(T1.VALOR_ORIGINAL)
+     FROM VW_TITULOS_COMPRADOR T1
+     WHERE T1.CONTA = T0.CONTA
+       AND T1.DATA_PROGRAMADA BETWEEN :DT_INICIO AND :DT_FIM
+    ) AS TOTAL_VALOR,
+
+    :SUPRIMIR AS FLAG_SUPRESSAO
+
+FROM VW_TITULOS_COMPRADOR T0
 WHERE 
-    T0.DTAPROGRAMADA BETWEEN :DT1 AND :DT2
+    T0.DATA_PROGRAMADA BETWEEN :DT_INICIO AND :DT_FIM
     AND T0.EMPRESA = CASE 
-                        WHEN :LS1 = 'TODAS' THEN T0.EMPRESA 
-                        ELSE :LS1 
+                        WHEN :EMPRESA = 'TODAS' THEN T0.EMPRESA 
+                        ELSE :EMPRESA 
                      END
-    AND T0.EXC_ESPECIES = CASE 
-                              WHEN :LS2 = 'TODAS' THEN T0.EXC_ESPECIES 
-                              ELSE '1' 
-                          END
-    AND (
-        (:LS3 = 'A' AND T0.ABERTOQUITADO = 'A') OR
-        (:LS3 = 'Q' AND T0.ABERTOQUITADO = 'Q') OR
-        (:LS3 NOT IN ('A', 'Q')) 
-    )
+    AND T0.STATUS = CASE 
+                        WHEN :SITUACAO IN ('A', 'Q') THEN :SITUACAO 
+                        ELSE T0.STATUS 
+                    END
     AND T0.COMPRADOR = CASE 
-                          WHEN :LS4 = 'TODOS' THEN T0.COMPRADOR 
-                          ELSE :LS4 
-                       END
-    AND (
-        (:NR1 = 1 AND T0.DTAPROGRAMADA = T0.DATAINCLPROG)
-        OR (:NR1 != 1 AND T0.DATAINCLPROG <> T0.DTAPROGRAMADA)
-    )
-;
+                          WHEN :COMPRADOR = 'TODOS' THEN T0.COMPRADOR 
+                          ELSE :COMPRADOR 
+                       END;
